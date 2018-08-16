846d1ebc73797316b97477d7f02c5d7ac6d05709
Mindas <mindaugas.lickus@webelevate.ie>
2018-08-16 09:13:39 +0100

    registration

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': 'off',//todo
+    'arrow-body-style': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
